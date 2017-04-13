969cb261688b7327c710534e17c91d3cfc8d9106
fengcheng <lufengc@qq.com>
2017-04-13 19:02:46 +0800

    config server

--- cloud-config-client/src/main/java/com/dsjk/config/client/ConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/dsjk/config/client/ConfigClientApplication.java
@@ -2,7 +2,9 @@ package com.dsjk.config.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
