56980ce4ef0cb7356ac4e47eca13401d2b2a9ef6
fengcheng <lufengc@qq.com>
2017-04-16 16:04:21 +0800

    config server

--- cloud-config-server/src/main/java/com/dsjk/config/ConfigApplication.java
+++ cloud-config-server/src/main/java/com/dsjk/config/ConfigApplication.java
@@ -1,7 +1,7 @@
 package com.dsjk.config;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -11,6 +11,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        new SpringApplicationBuilder(ConfigApplication.class).web(true).run(args);
     }
 }
