e18cc2605acff5b929d3f0bcb1c19f56c85e2723
cc <lufengc@163.com>
2017-05-06 00:25:59 +0800

    init commit

---
+++ cloud-security/src/main/java/com/dsjk/security/SecurityApplication.java
@@ -0,0 +1,15 @@
+package com.dsjk.security;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
