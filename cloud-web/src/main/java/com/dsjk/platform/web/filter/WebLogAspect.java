05587eda9312eebd31b72925fdfeea59bac02a10
fengcheng <lufengcheng@bdfint.com>
2018-01-14 13:13:46 +0800

    init

--- cloud-web/src/main/java/com/dsjk/platform/web/filter/WebLogAspect.java
+++ cloud-web/src/main/java/com/dsjk/platform/web/filter/WebLogAspect.java
@@ -1,17 +1,11 @@
 package com.dsjk.platform.web.filter;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 
 /**
  * @author fengcheng
@@ -29,19 +23,12 @@ public class WebLogAspect {
     }
 
     @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) {
+    public void doBefore() {
         startTime.set(System.currentTimeMillis());
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-        // 记录下请求内容
-        logger.info("URL : " + request.getRequestURL().toString());
-        logger.info("HTTP_METHOD : " + request.getMethod());
-        logger.info("IP : " + request.getRemoteAddr());
-        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
     }
 
-    @AfterReturning(returning = "ret", pointcut = "webLog()")
-    public void doAfterReturning(Object ret) {
-        logger.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+    @AfterReturning(returning = "result", pointcut = "webLog()")
+    public void doAfterReturning(Object result) {
+        logger.info("spend time : " + (System.currentTimeMillis() - startTime.get()));
     }
 }
